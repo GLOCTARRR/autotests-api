8d2fa490bba062af190780d4b2126a74f30ffe66
gloctarr <gloctarr@yandex.ru>
2026-03-10 23:50:07 +0300

    Введение в HTTPX.Практическое задание: работа с HTTPX

--- httpx_client/httpx_get_user_me.py
+++ httpx_client/httpx_get_user_me.py
@@ -4,14 +4,12 @@ from httpx_client.constants import HOST
 
 
 def login(email, password):
-    payload = {
-        "email": email,
-        "password": password
-    }
+    payload = {"email": email, "password": password}
     response = httpx.post(f"{HOST}/api/v1/authentication/login", json=payload)
     print(response.json())
     print(response.status_code)
     return response.json()
+
 
 def me(access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
@@ -21,7 +19,7 @@ def me(access_token):
     print(response.status_code)
     return response.json()
 
+
 if __name__ == "__main__":
     auth_data = login("gloctarr@example.com", "123456")
     me(auth_data["token"]["accessToken"])
-
