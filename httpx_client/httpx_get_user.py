8fed8b2817e26ba273bcf4b72a5984e8b32535ca
gloctarr <gloctarr@yandex.ru>
2026-03-11 00:27:49 +0300

    Введение в HTTPX.Практическое задание: работа с HTTPX

--- httpx_client/httpx_get_user.py
+++ httpx_client/httpx_get_user.py
@@ -1,5 +1,25 @@
-On branch main
-Changes to be committed:
-  (use "git restore --staged <file>..." to unstage)
-	deleted:    httpx_client/httpx_get_user_me.py
+import httpx
 
+from httpx_client.constants import HOST
+
+
+def login(email, password):
+    payload = {"email": email, "password": password}
+    response = httpx.post(f"{HOST}/api/v1/authentication/login", json=payload)
+    print(response.json())
+    print(response.status_code)
+    return response.json()
+
+
+def me(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = httpx.get(f"{HOST}/api/v1/users/me", headers=headers)
+    print(response.json())
+    print(response.status_code)
+    return response.json()
+
+
+if __name__ == "__main__":
+    auth_data = login("gloctarr@example.com", "123456")
+    me(auth_data["token"]["accessToken"])
