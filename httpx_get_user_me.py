38cf8e89c36ac206a67d58bb03026a07cba2287f
gloctarr <gloctarr@yandex.ru>
2026-03-12 13:53:44 +0300

    Знакомство с API клиентами.Практика: Написание API клиента

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -1,11 +1,11 @@
 import httpx
 
-from httpx_client.constants import HOST, PASSWORD
-
 
 def login(email: str, password: str) -> dict:
     payload = {"email": email, "password": password}
-    response = httpx.post(f"{HOST}/api/v1/authentication/login", json=payload)
+    response = httpx.post(
+        "http://localhost:8000/api/v1/authentication/login", json=payload
+    )
     print(response.json())
     print(response.status_code)
     return response.json()
@@ -13,13 +13,12 @@ def login(email: str, password: str) -> dict:
 
 def me(access_token: str):
     headers = {"Authorization": f"Bearer {access_token}"}
-
-    response = httpx.get(f"{HOST}/api/v1/users/me", headers=headers)
+    response = httpx.get("http://localhost:8000/api/v1/users/me", headers=headers)
     print(response.json())
     print(response.status_code)
     return response.json()
 
 
 if __name__ == "__main__":
-    auth_data = login("gloctarr@example.com", PASSWORD)
+    auth_data = login("gloctarr@example.com", "123456")
     me(auth_data["token"]["accessToken"])
--- httpx_update_user.py
+++ httpx_update_user.py
@@ -1,13 +1,14 @@
 import httpx
 
 from utils import data_utils
-from httpx_client.constants import HOST, PASSWORD
 
 
 def login(email: str, password: str) -> dict:
     print("___login___:")
     payload = {"email": email, "password": password}
-    response = httpx.post(f"{HOST}/api/v1/authentication/login", json=payload)
+    response = httpx.post(
+        "http://localhost:8000/api/v1/authentication/login", json=payload
+    )
     print(response.json())
     print(response.status_code)
     return response.json()
@@ -15,7 +16,7 @@ def login(email: str, password: str) -> dict:
 
 def create_user(user_data: dict) -> dict:
     print("___create_user___:")
-    response = httpx.post(f"{HOST}/api/v1/users", json=user_data)
+    response = httpx.post("http://localhost:8000/api/v1/users", json=user_data)
     print(response.json())
     print(response.status_code)
     return response.json()
@@ -26,7 +27,7 @@ def update_user(user_id: str, user_data: dict, access_token: str) -> dict:
     headers = {"Authorization": f"Bearer {access_token}"}
 
     response = httpx.patch(
-        f"{HOST}/api/v1/users/{user_id}", json=user_data, headers=headers
+        f"http://localhost:8000/api/v1/users/{user_id}", json=user_data, headers=headers
     )
     print(response.json())
     print(response.status_code)
@@ -34,16 +35,17 @@ def update_user(user_id: str, user_data: dict, access_token: str) -> dict:
 
 
 if __name__ == "__main__":
+    password = "123456"
     user_data = {
         "email": data_utils.generate_email(),
-        "password": PASSWORD,
+        "password": password,
         "lastName": data_utils.generate_string(),
         "firstName": data_utils.generate_string(),
         "middleName": data_utils.generate_string(),
     }
 
     created_user_data = create_user(user_data)
-    auth_data = login(user_data["email"], PASSWORD)
+    auth_data = login(user_data["email"], password)
 
     new_user_data = {
         "email": data_utils.generate_email(),
